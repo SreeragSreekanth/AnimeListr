c3965afaa3455ad4cc92201fde6dd3a1d8115a44
Sreerag <srrgsreekanth001@gmail.com>
2025-06-21 12:28:22 +0530

    added staticfilestorage

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # For production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
